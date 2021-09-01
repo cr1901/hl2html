8ab9559182189042df7d047721cb7afc1e109823
William D. Jones <thor0505@comcast.net>
2021-09-01 13:59:13 -0400

    Start writing out main application.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,42 @@ mod ast;
 mod lexer;
 mod parser;
 
+use ast::HotlistError;
+use lexer::LexerError;
 use parser::parse_hotlist_from_file;
 
+use argh::FromArgs;
+use lalrpop_util::ParseError;
+use std::error::Error;
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// A command with positional arguments.
+struct HotlistArgs {
+    #[argh(positional)]
+    path: String,
+}
+
 fn main() {
+    let args: HotlistArgs = argh::from_env();
     let mut in_buf = String::new();
 
-    let hotlist = parse_hotlist_from_file("", &mut in_buf).unwrap();
+    let hotlist = match parse_hotlist_from_file(&args.path, &mut in_buf) {
+        Ok(hl) => hl,
+        Err(e) => {
+            println!("Error while parsing hotlist file {}:", args.path);
+            print_error_and_exit(e, 1);
+        }
+    };
+}
 
-    unimplemented!()
+fn print_error_and_exit<'a>(err: Box<dyn Error + Send + Sync + 'a>, exit_code: i32) -> ! {
+    let mut printing = true;
+    let mut curr_err = err;
+
+    while printing {
+        println!("{}", curr_err);
+        printing = false;
+    }
+
+    std::process::exit(exit_code)
 }
