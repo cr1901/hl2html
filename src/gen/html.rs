0989c83bf623a8af8df151e7cddd001fd0df1452
William D. Jones <thor0505@comcast.net>
2021-09-05 23:22:14 -0400

    Move HTML single file emitter to own file.

--- src/gen/html.rs
+++ src/gen/html.rs
@@ -1,6 +1,9 @@
-use super::{traverse_hotlist, Visitor};
-use crate::ast::{EntryKind, Folder, Hotlist, Note};
+mod single;
+
+use crate::ast::Hotlist;
 use crate::error::Error;
+use single::SingleEmitter;
+use super::traverse_hotlist;
 
 use std::fs::File;
 use std::io::{self, BufWriter, Write};
@@ -18,30 +21,19 @@ pub fn emit<T: AsRef<Path>>(
         Box::new(BufWriter::new(io::stdout()))
     };
 
-    let mut emitter = HtmlEmitter::new(out_handle);
-    traverse_hotlist(hl, &mut emitter)?;
-
-    let mut out_handle = emitter.into_inner();
-    out_handle.flush()?;
+    if multi {
+        return Err("multiple-file HTML rendering not implemented yet".into());
+    } else {
+        let mut emitter = SingleEmitter::new(out_handle);
+        traverse_hotlist(hl, &mut emitter)?;
+        let mut out_handle = emitter.into_inner();
+        out_handle.flush()?;
+    }
 
     Ok(())
 }
 
-struct HtmlEmitter<W>
-where
-    W: Write,
-{
-    buf: W,
-}
-
-impl<W> HtmlEmitter<W>
-where
-    W: Write,
-{
-    fn new(buf: W) -> Self {
-        Self { buf }
-    }
-
+trait HtmlEscapeWrite: Write {
     fn write_with_escapes(&mut self, raw: &str) -> io::Result<()> {
         let mut possible_newline = false;
         for c in raw.chars() {
@@ -50,26 +42,26 @@ where
                     possible_newline = true;
                 }
                 '\x02' if possible_newline => {
-                    write!(self.buf, "</p>\n{:1$}<p>", " ", 4)?;
+                    write!(self, "</p>\n{:1$}<p>", " ", 4)?;
                     possible_newline = false;
                 }
                 '<' => {
-                    write!(self.buf, "&lt;")?;
+                    write!(self, "&lt;")?;
                 }
                 '>' => {
-                    write!(self.buf, "&gt;")?;
+                    write!(self, "&gt;")?;
                 }
                 '"' => {
-                    write!(self.buf, "&quot;")?;
+                    write!(self, "&quot;")?;
                 }
                 '&' => {
-                    write!(self.buf, "&amp;")?;
+                    write!(self, "&amp;")?;
                 }
                 '\'' => {
-                    write!(self.buf, "&apos;")?;
+                    write!(self, "&apos;")?;
                 }
                 _ => {
-                    write!(self.buf, "{}", c)?;
+                    write!(self, "{}", c)?;
                 }
             }
 
@@ -84,117 +76,6 @@ where
 
         Ok(())
     }
-
-    fn into_inner(self) -> W {
-        self.buf
-    }
 }
 
-impl<W> Visitor for HtmlEmitter<W>
-where
-    W: Write,
-{
-    fn visit_folder_empty(
-        &mut self,
-        f: &Folder,
-    ) -> Result<(), Error<'static>> {
-        write!(self.buf, "{:1$}<h2>Folder {2}</h2>\n", " ", 4, f.name)?;
-        write!(self.buf, "{:1$}<ul>\n", " ", 4)?;
-        write!(self.buf, "{:1$}<li>ID: {2}</li>\n", " ", 6, f.id)?;
-        write!(self.buf, "{:1$}<li>UUID: {2}</li>\n", " ", 6, f.uuid)?;
-        write!(self.buf, "{:1$}<li>Created: {2}</li>\n", " ", 6, f.timestamp)?;
-        write!(self.buf, "{:1$}</ul>\n", " ", 4)?;
-
-        write!(self.buf, "{:1$}<p>No Entries<p>\n", " ", 4)?;
-
-        write!(self.buf, "\n")?;
-        Ok(())
-    }
-
-    fn visit_folder_pre(
-        &mut self,
-        f: &Folder,
-    ) -> Result<(), Error<'static>> {
-        write!(self.buf, "{:1$}<h2>Folder {2}</h2>\n", " ", 4, f.name)?;
-        write!(self.buf, "{:1$}<ul>\n", " ", 4)?;
-        write!(self.buf, "{:1$}<li>ID: {2}</li>\n", " ", 6, f.id)?;
-        write!(self.buf, "{:1$}<li>UUID: {2}</li>\n", " ", 6, f.uuid)?;
-        write!(self.buf, "{:1$}<li>Created: {2}</li>\n", " ", 6, f.timestamp)?;
-        write!(self.buf, "{:1$}</ul>\n", " ", 4)?;
-
-        write!(self.buf, "\n")?;
-        Ok(())
-    }
-
-    fn visit_folder_post(
-        &mut self,
-        f: &Folder,
-    ) -> Result<(), Error<'static>> {
-        write!(self.buf, "{:1$}<p>End Folder {2}</p>\n", " ", 4, f.name)?;
-        write!(self.buf, "\n")?;
-        Ok(())
-    }
-
-    fn visit_note(&mut self, n: &Note) -> Result<(), Error<'static>> {
-        write!(self.buf, "{:1$}<h2>Note {2}</h2>\n", " ", 4, n.id)?;
-        write!(self.buf, "{:1$}<ul>\n", " ", 4)?;
-        write!(self.buf, "{:1$}<li>UUID: {2}</li>\n", " ", 6, n.uuid)?;
-
-        // without "&": cannot move out of `n.url.0` which is behind a shared reference
-        if let Some(u) = &n.url {
-            write!(self.buf, "{:1$}<li>URL: <a href=\"{2}\">{2}</a></li>\n", " ", 6, u)?;
-        } else {
-            write!(self.buf, "{:1$}<li>URL: None</li>\n", " ", 6)?;
-        }
-
-        write!(self.buf, "{:1$}<li>Created: {2}</li>\n", " ", 6, n.timestamp)?;
-        write!(self.buf, "{:1$}</ul>\n", " ", 4)?;
-
-        if let Some(nbody) = n.contents {
-            write!(self.buf, "{:1$}<p>", " ", 4)?;
-            self.write_with_escapes(&nbody)?;
-            write!(self.buf, "<p>\n")?;
-        }
-
-        write!(self.buf, "\n")?;
-        Ok(())
-    }
-
-    fn visit_root_pre(
-        &mut self,
-        hl: &Hotlist,
-    ) -> Result<(), Error<'static>> {
-        write!(
-            self.buf,
-            r#"<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Opera Hotlist</title>
-  </head>
-  <body>
-"#
-        )?;
-
-        write!(
-            self.buf,
-            "{:1$}<h1>Opera Hotlist Version {2}</h1>\n",
-            " ", 4, hl.version
-        )?;
-
-        Ok(())
-    }
-
-    fn visit_root_post(
-        &mut self,
-        _hl: &Hotlist,
-    ) -> Result<(), Error<'static>> {
-        write!(
-            self.buf,
-            r#"  </body>
-</html>
-"#
-        )?;
-
-        Ok(())
-    }
-}
+impl<W> HtmlEscapeWrite for W where W: Write { }
---
+++ src/gen/html/single.rs
@@ -0,0 +1,135 @@
+use crate::ast::{Folder, Hotlist, Note};
+use crate::error::Error;
+use crate::gen::Visitor;
+use super::HtmlEscapeWrite;
+
+use std::io::{self, Write};
+
+pub struct SingleEmitter<W>
+where
+    W: Write,
+{
+    buf: W,
+}
+
+impl<W> SingleEmitter<W>
+where
+    W: Write,
+{
+    pub fn new(buf: W) -> Self {
+        Self { buf }
+    }
+
+    pub fn into_inner(self) -> W {
+        self.buf
+    }
+}
+
+impl<W> Visitor for SingleEmitter<W>
+where
+    W: Write,
+{
+    fn visit_folder_empty(
+        &mut self,
+        f: &Folder,
+    ) -> Result<(), Error<'static>> {
+        write!(self.buf, "{:1$}<h2>Folder {2}</h2>\n", " ", 4, f.name)?;
+        write!(self.buf, "{:1$}<ul>\n", " ", 4)?;
+        write!(self.buf, "{:1$}<li>ID: {2}</li>\n", " ", 6, f.id)?;
+        write!(self.buf, "{:1$}<li>UUID: {2}</li>\n", " ", 6, f.uuid)?;
+        write!(self.buf, "{:1$}<li>Created: {2}</li>\n", " ", 6, f.timestamp)?;
+        write!(self.buf, "{:1$}</ul>\n", " ", 4)?;
+
+        write!(self.buf, "{:1$}<p>No Entries<p>\n", " ", 4)?;
+
+        write!(self.buf, "\n")?;
+        Ok(())
+    }
+
+    fn visit_folder_pre(
+        &mut self,
+        f: &Folder,
+    ) -> Result<(), Error<'static>> {
+        write!(self.buf, "{:1$}<h2>Folder {2}</h2>\n", " ", 4, f.name)?;
+        write!(self.buf, "{:1$}<ul>\n", " ", 4)?;
+        write!(self.buf, "{:1$}<li>ID: {2}</li>\n", " ", 6, f.id)?;
+        write!(self.buf, "{:1$}<li>UUID: {2}</li>\n", " ", 6, f.uuid)?;
+        write!(self.buf, "{:1$}<li>Created: {2}</li>\n", " ", 6, f.timestamp)?;
+        write!(self.buf, "{:1$}</ul>\n", " ", 4)?;
+
+        write!(self.buf, "\n")?;
+        Ok(())
+    }
+
+    fn visit_folder_post(
+        &mut self,
+        f: &Folder,
+    ) -> Result<(), Error<'static>> {
+        write!(self.buf, "{:1$}<p>End Folder {2}</p>\n", " ", 4, f.name)?;
+        write!(self.buf, "\n")?;
+        Ok(())
+    }
+
+    fn visit_note(&mut self, n: &Note) -> Result<(), Error<'static>> {
+        write!(self.buf, "{:1$}<h2>Note {2}</h2>\n", " ", 4, n.id)?;
+        write!(self.buf, "{:1$}<ul>\n", " ", 4)?;
+        write!(self.buf, "{:1$}<li>UUID: {2}</li>\n", " ", 6, n.uuid)?;
+
+        // without "&": cannot move out of `n.url.0` which is behind a shared reference
+        if let Some(u) = &n.url {
+            write!(self.buf, "{:1$}<li>URL: <a href=\"{2}\">{2}</a></li>\n", " ", 6, u)?;
+        } else {
+            write!(self.buf, "{:1$}<li>URL: None</li>\n", " ", 6)?;
+        }
+
+        write!(self.buf, "{:1$}<li>Created: {2}</li>\n", " ", 6, n.timestamp)?;
+        write!(self.buf, "{:1$}</ul>\n", " ", 4)?;
+
+        if let Some(nbody) = n.contents {
+            write!(self.buf, "{:1$}<p>", " ", 4)?;
+            self.buf.write_with_escapes(&nbody)?;
+            write!(self.buf, "<p>\n")?;
+        }
+
+        write!(self.buf, "\n")?;
+        Ok(())
+    }
+
+    fn visit_root_pre(
+        &mut self,
+        hl: &Hotlist,
+    ) -> Result<(), Error<'static>> {
+        write!(
+            self.buf,
+            r#"<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Opera Hotlist</title>
+  </head>
+  <body>
+"#
+        )?;
+
+        write!(
+            self.buf,
+            "{:1$}<h1>Opera Hotlist Version {2}</h1>\n",
+            " ", 4, hl.version
+        )?;
+
+        Ok(())
+    }
+
+    fn visit_root_post(
+        &mut self,
+        _hl: &Hotlist,
+    ) -> Result<(), Error<'static>> {
+        write!(
+            self.buf,
+            r#"  </body>
+</html>
+"#
+        )?;
+
+        Ok(())
+    }
+}
