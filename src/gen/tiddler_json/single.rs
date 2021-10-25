08ccd29d3920d636ac99b8604eb2bb545f52026d
William D. Jones <thor0505@comcast.net>
2021-10-25 10:35:44 -0400

    Rename StringRefs to SerializeType, Id variant to U32.

--- src/gen/tiddler_json/single.rs
+++ src/gen/tiddler_json/single.rs
@@ -10,31 +10,31 @@ use chrono::{DateTime, Utc};
 use serde::{Serialize, Serializer};
 
 #[derive(Debug)]
-enum StringRefs<'arena, 'input> {
+enum SerializeType<'arena, 'input> {
     Input(&'input str),
     Arena(bumpalo::collections::String<'arena>),
     DateTime(super::DateTime),
-    Id(u32),
+    U32(u32),
     Uuid(super::Uuid)
 }
 
-impl<'arena, 'input> Serialize for StringRefs<'arena, 'input> {
+impl<'arena, 'input> Serialize for SerializeType<'arena, 'input> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
         match self {
-            StringRefs::Input(s) => s.serialize(serializer),
-            StringRefs::Arena(a) => a.serialize(serializer),
-            StringRefs::DateTime(d) => d.serialize(serializer),
-            StringRefs::Id(i) => i.serialize(serializer),
-            StringRefs::Uuid(u) => u.serialize(serializer),
+            SerializeType::Input(s) => s.serialize(serializer),
+            SerializeType::Arena(a) => a.serialize(serializer),
+            SerializeType::DateTime(d) => d.serialize(serializer),
+            SerializeType::U32(i) => i.serialize(serializer),
+            SerializeType::Uuid(u) => u.serialize(serializer),
         }
     }
 }
 
 pub struct SingleGenerator<'arena, 'input> {
-    json: Vec<HashMap<&'static str, StringRefs<'arena, 'input>>>,
+    json: Vec<HashMap<&'static str, SerializeType<'arena, 'input>>>,
     root: PathBuf,
     arena: &'arena Bump,
     now: DateTime<Utc>,
@@ -51,7 +51,7 @@ impl<'arena, 'input> Serialize for SingleGenerator<'arena, 'input> {
 
 impl<'arena, 'input> SingleGenerator<'arena, 'input> {
     pub fn new(arena: &'arena Bump) -> Self {
-        let json = Vec::<HashMap<&'static str, StringRefs>>::new();
+        let json = Vec::<HashMap<&'static str, SerializeType>>::new();
         let root = PathBuf::new();
         let now = Utc::now();
         Self {
@@ -81,16 +81,16 @@ impl<'a, 'arena, 'input> Visitor<'a, 'input> for SingleGenerator<'arena, 'input>
     fn visit_note(&mut self, n: &'a Note<'input>) -> Result<(), Error<'static>> {
         let mut entry = HashMap::new();
 
-        entry.insert("text", StringRefs::Input(n.contents.unwrap_or("")));
+        entry.insert("text", SerializeType::Input(n.contents.unwrap_or("")));
 
         // Hotlist-specific
-        entry.insert("uuid", StringRefs::Uuid(n.uuid.into()));
+        entry.insert("uuid", SerializeType::Uuid(n.uuid.into()));
 
-        entry.insert("timestamp", StringRefs::DateTime(n.timestamp.into()));
+        entry.insert("timestamp", SerializeType::DateTime(n.timestamp.into()));
 
-        entry.insert("created", StringRefs::DateTime(self.now.into()));
-        entry.insert("modified", StringRefs::DateTime(self.now.into()));
-        entry.insert("tags", StringRefs::Input("opera"));
+        entry.insert("created", SerializeType::DateTime(self.now.into()));
+        entry.insert("modified", SerializeType::DateTime(self.now.into()));
+        entry.insert("tags", SerializeType::Input("opera"));
 
         let url = match &n.url {
             Some(u) => {
@@ -102,9 +102,9 @@ impl<'a, 'arena, 'input> Visitor<'a, 'input> for SingleGenerator<'arena, 'input>
         // TODO: When building the landing page, show URL for each entry but truncate to
         // a reasonable number of characters.
         let title = bumpalo::format!(in &self.arena, "Note {}", n.id);
-        entry.insert("title", StringRefs::Arena(title));
-        entry.insert("url", StringRefs::Arena(url));
-        entry.insert("id", StringRefs::Id(n.id));
+        entry.insert("title", SerializeType::Arena(title));
+        entry.insert("url", SerializeType::Arena(url));
+        entry.insert("id", SerializeType::U32(n.id));
 
         let folder = bumpalo::format!(in &self.arena,
          "{}", self.root
@@ -112,8 +112,8 @@ impl<'a, 'arena, 'input> Visitor<'a, 'input> for SingleGenerator<'arena, 'input>
                    .ok_or(format!("invalid path: {}",
                                   self.root.to_string_lossy()))?
         );
-        entry.insert("folder", StringRefs::Arena(folder));
-        entry.insert("commit-sha", StringRefs::Input(env!("VERGEN_GIT_SHA")));
+        entry.insert("folder", SerializeType::Arena(folder));
+        entry.insert("commit-sha", SerializeType::Input(env!("VERGEN_GIT_SHA")));
 
         self.json.push(entry);
 
@@ -128,19 +128,19 @@ impl<'a, 'arena, 'input> Visitor<'a, 'input> for SingleGenerator<'arena, 'input>
         // The final, main tiddler is the landing page.
         let mut entry = HashMap::new();
 
-        entry.insert("title", StringRefs::Input("Opera Notes"));
+        entry.insert("title", SerializeType::Input("Opera Notes"));
         entry.insert(
             "text",
-            StringRefs::Input(
+            SerializeType::Input(
                 "<$list filter=\"[tag[opera]nsort[id]]\">\n<$link/> ({{!!url}})<br/>\n</$list>",
             ),
         );
-        entry.insert("tags", StringRefs::Input("opera"));
+        entry.insert("tags", SerializeType::Input("opera"));
 
-        entry.insert("created", StringRefs::DateTime(self.now.into()));
-        entry.insert("modified", StringRefs::DateTime(self.now.into()));
+        entry.insert("created", SerializeType::DateTime(self.now.into()));
+        entry.insert("modified", SerializeType::DateTime(self.now.into()));
 
-        entry.insert("commit-sha", StringRefs::Input(env!("VERGEN_GIT_SHA")));
+        entry.insert("commit-sha", SerializeType::Input(env!("VERGEN_GIT_SHA")));
 
         self.json.push(entry);
 
